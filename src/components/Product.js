762e29e1d320f409d1e982baa17700ba372770d2
lin chihhao <iita71737@gmail.com>
2021-10-23 16:16:29 +0800

    added to carts

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { formatPrice } from 'commons/helper';
 import Panel from 'components/Panel';
 import EditInventory from 'components/EditInventory';
+import axios from 'commons/axios';
+import { toast } from 'react-toastify';
 
 class Product extends React.Component {
 
@@ -18,6 +20,31 @@ class Product extends React.Component {
                 }
             }
         });
+    };
+
+    addCart = async () => {
+        try {
+            const { id, name, image, price } = this.props.product;
+            const res = await axios.get(`/carts?productId=${id}`);
+            const carts = res.data;
+            if (carts && carts.length > 0) {
+                const cart = carts[0];
+                cart.mount += 1;
+                await axios.put(`/carts/${cart.id}`, cart);
+            } else {
+                const cart = {
+                    productId: id,
+                    name,
+                    image,
+                    price,
+                    mount: 1
+                };
+                await axios.post('/carts', cart);
+            }
+            toast.success('Add Cart Success');
+        } catch (error) {
+            toast.error('Add Cart Failed');
+        }
     };
 
     render() {
@@ -47,7 +74,7 @@ class Product extends React.Component {
                 </div>
                 <div className="p-footer">
                     <p className="price">{formatPrice(price)}</p>
-                    <button className="add-cart" disabled={status === 'unavailable'}>
+                    <button className="add-cart" disabled={status === 'unavailable'} onClick={this.addCart}>
                         <i className="fas fa-shopping-cart"></i>
                         <i className="fas fa-exclamation"></i>
                     </button>
