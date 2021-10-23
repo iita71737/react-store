0ad2e0df792182c5c03bdc15dbe62054db57d7f5
lin chihhao <iita71737@gmail.com>
2021-10-23 20:53:35 +0800

    added Update & Delete in Carts

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { formatPrice } from 'commons/helper';
+import { useState } from 'react';
+import axios from 'commons/axios';
 
 const CartItem = props => {
-    const { name, image, price, mount } = props.cart || {};
+    const [mount, setMount] = useState(props.cart.mount);
+    const { id, name, image, price } = props.cart || {};
     const sumPrice = formatPrice(mount * parseInt(price));
+
+    const handleChange = e => {
+        const _mount = parseInt(e.target.value);
+        setMount(_mount);
+        const newCart = {
+            ...props.cart,
+            mount: _mount
+        };
+        axios.put(`/carts/${id}`, newCart).then(res => {
+            props.updateCart(newCart);
+        });
+    };
+
+    const deleteCart = () => {
+        axios.delete(`/carts/${id}`).then(res => {
+            props.deleteCart(props.cart);
+        });
+    };
 
     return (
         <div className="columns is-vcentered">
             <div className="column is-narrow">
-                <span className="close">X</span>
+                <span className="close" onClick={deleteCart}>X</span>
             </div>
             <div className="column is-narrow">
                 <img src={image} alt={name} width="100" />
@@ -18,7 +39,11 @@ const CartItem = props => {
                 <span className="price">{formatPrice(price)}</span>
             </div>
             <div className="column">
-                <input type="number" className="input num-input" defaultValue={mount} />
+                <input type="number"
+                    className="input num-input"
+                    min={1}
+                    value={mount}
+                    onChange={handleChange} />
             </div>
             <div className="column">
                 <span className="sum-price">{sumPrice}</span>
